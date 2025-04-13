97bb228f21105cdc010a5067e1d8164ca6a9b5c7
Andrea Fernández <andreafernez@estudiantec.cr>
2025-04-13 14:19:40 -0600

    Nombres de los integrantes

---
+++ src/Aplicacion/Main.java
@@ -0,0 +1,15 @@
+package Aplicacion;
+
+/**
+ * Proyecto 1 Programacion Orientada a Objetos
+ * @author Mayte Andrea Fernandez Angulo 2021040818
+ * @author Jose David Fernández Ampie 2020032018
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
