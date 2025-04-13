2ea062d26b71a170e39175feffa579d55f9617bc
Andrea Fernández <andreafernez@estudiantec.cr>
2025-04-13 14:59:04 -0600

    Atributos de Abogados

---
+++ src/Conceptos/Abogados.java
@@ -0,0 +1,14 @@
+package Conceptos;
+
+import java.util.ArrayList;
+
+public class Abogados {
+    
+    String IdAbogado;
+    String nombreAbogado;
+    String telefonoAbogado;
+    String puestoAbogado;
+    
+    ArrayList<Servicios> listaServiciosAbogado = new ArrayList();
+    
+}
