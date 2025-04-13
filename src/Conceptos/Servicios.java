85e143b37fe0eb172228a63685c4d0a586a078dd
Andrea Fernández <andreafernez@estudiantec.cr>
2025-04-13 15:35:44 -0600

    Clase de Servicios preparada
    Constructor, setters y getters

--- src/Conceptos/Servicios.java
+++ src/Conceptos/Servicios.java
@@ -2,8 +2,50 @@ package Conceptos;
 
 public class Servicios {
     
-    String IdServicio;
-    String nombreServicio;
-    int precioServicio;
+    private String idServicio;
+    private String nombreServicio;
+    private int precioServicio;
+
+    // constructor de Servicios
+    public Servicios(String idServicio, String nombreServicio, int precioServicio) {
+        this.idServicio = idServicio;
+        this.nombreServicio = nombreServicio;
+        this.precioServicio = precioServicio;
+    }
+    
+    // constructor por default
+    public Servicios() {
+    }
+    
+    // ********** setters y getters **********
+
+    public void setidServicio(String idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public void setNombreServicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+    }
+
+    public void setPrecioServicio(int precioServicio) {
+        this.precioServicio = precioServicio;
+    }
+
+    public String getidServicio() {
+        return idServicio;
+    }
+
+    public String getNombreServicio() {
+        return nombreServicio;
+    }
+
+    public int getPrecioServicio() {
+        return precioServicio;
+    }
+    
+    @Override
+    public String toString(){
+        return nombreServicio + " " + idServicio + ".";
+    }
     
 }
