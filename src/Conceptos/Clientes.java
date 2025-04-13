47c1441f2329db00d40ca292f4dbf5d227f752fe
Andrea Fernández <andreafernez@estudiantec.cr>
2025-04-13 15:35:31 -0600

    Clase de clientes preparada
    Constructor, setters y getters

--- src/Conceptos/Clientes.java
+++ src/Conceptos/Clientes.java
@@ -2,9 +2,59 @@ package Conceptos;
 
 public class Clientes {
     
-    String IdCliente;
-    String nombreCliente;
-    String telefonoCliente;
-    String emailCliente;
+    private String idCliente;
+    private String nombreCliente;
+    private String telefonoCliente;
+    private String emailCliente;
+
+    // Constructor de Clientes
+    public Clientes(String idCliente, String nombreCliente, String telefonoCliente, String emailCliente) {
+        this.idCliente = idCliente;
+        this.nombreCliente = nombreCliente;
+        this.telefonoCliente = telefonoCliente;
+        this.emailCliente = emailCliente;
+    }
+
+    // Constructor por default
+    public Clientes() {
+    }
     
+    // ********** setters y getters **********
+
+    public void setidCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public void setTelefonoCliente(String telefonoCliente) {
+        this.telefonoCliente = telefonoCliente;
+    }
+
+    public void setEmailCliente(String emailCliente) {
+        this.emailCliente = emailCliente;
+    }
+
+    public String getidCliente() {
+        return idCliente;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public String getTelefonoCliente() {
+        return telefonoCliente;
+    }
+
+    public String getEmailCliente() {
+        return emailCliente;
+    }
+    
+    @Override
+    public String toString(){
+        return nombreCliente + " " + idCliente + ".";
+    }
 }
